bf1b9d25c777b968ff16b07d3d20311132875d4d
vutrngkien <vutrungkien2k@gmail.com>
2023-09-10 19:45:17 +0700

    add models

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ from flask import Flask, request, jsonify, abort
 from sqlalchemy import exc
 import json
 from flask_cors import CORS
+from models import setup_db, db_drop_and_create_all
 
 app = Flask(__name__)
-# setup_db(app)
+setup_db(app)
 CORS(app)
+
+db_drop_and_create_all()
 
 # Define a route and a view function
 @app.route('/')
